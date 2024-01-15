d5377f84295295857f706ac727e312697f9ccff5
thflf <thflf93@gmail.com>
2024-01-15 11:40:15 +0900

    feat: spring batch 병렬 처리 변경

--- src/main/java/com/ll/springbatch20240111/batch/Hello3JobConfig.java
+++ src/main/java/com/ll/springbatch20240111/batch/Hello3JobConfig.java
@@ -3,11 +3,14 @@ package com.ll.springbatch20240111.batch;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.builder.FlowJobBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.job.flow.support.SimpleFlow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -21,25 +24,32 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 public class Hello3JobConfig {
     @Bean
-    public Job hello3Job(JobRepository jobRepository, Flow hello3Flow, Step hello3Step3) {
-        return new JobBuilder("hello3Job", jobRepository)
-                .start(hello3Flow)
-                .next(hello3Step3)
-                .end() // Job 정의를 마칩니다.
+    public Job hello3Job(
+            JobRepository jobRepository,
+            Step hello3Step1,
+            Step hello3Step2,
+            Step hello3Step3
+    ) {
+        Flow flow1 = new FlowBuilder<SimpleFlow>("flow1")
+                .start(hello3Step1)
                 .build();
-    }
 
-    @Bean
-    public Flow hello3Flow(Step hello3Step1, Step hello3Step2) {
-        // Flow 객체를 생성하고 Step을 Flow로 변환합니다.
-        Flow flowStep1 = new FlowBuilder<Flow>("flowHello3Step1").from(hello3Step1).end();
-        Flow flowStep2 = new FlowBuilder<Flow>("flowHello3Step2").from(hello3Step2).end();
+        Flow flow2 = new FlowBuilder<SimpleFlow>("flow2")
+                .start(hello3Step2)
+                .build();
 
-        // FlowBuilder를 사용하여 병렬 실행을 구성합니다.
-        return new FlowBuilder<SimpleFlow>("hello3Flow")
-                .split(new SimpleAsyncTaskExecutor())
-                .add(flowStep1, flowStep2)
-                .end();
+        Flow parallelFlow = new FlowBuilder<SimpleFlow>("parallelFlow")
+                .split(new SimpleAsyncTaskExecutor()) // 병렬 실행을 위한 실행기 설정
+                .add(flow1, flow2)
+                .build();
+
+        FlowJobBuilder jobBuilder = new JobBuilder("hello3Job", jobRepository)
+                .start(parallelFlow)
+                .next(hello3Step3)
+                .end()
+                .incrementer(new RunIdIncrementer());
+
+        return jobBuilder.build();
     }
 
     @Bean
@@ -53,7 +63,7 @@ public class Hello3JobConfig {
     @Bean
     public Tasklet hello3Step1Tasklet() {
         return ((contribution, chunkContext) -> {
-            System.out.println("Hello World 3/1");
+            System.out.println("Hello World 3-1");
             return RepeatStatus.FINISHED;
         });
     }
@@ -69,11 +79,12 @@ public class Hello3JobConfig {
     @Bean
     public Tasklet hello3Step2Tasklet() {
         return ((contribution, chunkContext) -> {
-            System.out.println("Hello World 3/2");
+            System.out.println("Hello World 3-2");
             return RepeatStatus.FINISHED;
         });
     }
 
+    @JobScope
     @Bean
     public Step hello3Step3(JobRepository jobRepository, Tasklet hello3Step3Tasklet, PlatformTransactionManager platformTransactionManager) {
         return new StepBuilder("hello3Step3Tasklet", jobRepository)
@@ -85,7 +96,7 @@ public class Hello3JobConfig {
     @Bean
     public Tasklet hello3Step3Tasklet() {
         return ((contribution, chunkContext) -> {
-            System.out.println("Hello World 3/3");
+            System.out.println("Hello World 3-3");
             return RepeatStatus.FINISHED;
         });
     }
