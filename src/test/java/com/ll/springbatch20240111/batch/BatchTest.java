8bbd316348f701070d16c34ba2aacd12b8766bf0
thflf <thflf93@gmail.com>
2024-01-15 10:47:25 +0900

    feat: spring batch 병렬 처리

--- src/test/java/com/ll/springbatch20240111/batch/BatchTest.java
+++ src/test/java/com/ll/springbatch20240111/batch/BatchTest.java
@@ -16,6 +16,8 @@ public class BatchTest {
     private JobLauncherTestUtils helloJobLauncherTestUtils;
     @Autowired
     private JobLauncherTestUtils hello2JobLauncherTestUtils;
+    @Autowired
+    private JobLauncherTestUtils hello3JobLauncherTestUtils;
 
     @DisplayName("t1")
     @Test
@@ -27,5 +29,11 @@ public class BatchTest {
     @Test
     public void t2() throws Exception {
         hello2JobLauncherTestUtils.launchJob();
+    }
+
+    @DisplayName("t3")
+    @Test
+    public void t3() throws Exception {
+        hello3JobLauncherTestUtils.launchJob();
     }
 }
